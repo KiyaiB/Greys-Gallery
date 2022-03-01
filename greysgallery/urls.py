d9ba241245ddbc591f1320c1feb422ce405a020a
kiyaiB <babra.kiyai@student.moringaschool.com>
2022-03-01 09:46:16 +0300

    Modified urls.py

--- greysgallery/urls.py
+++ greysgallery/urls.py
@@ -15,8 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-from gallery.views import add_gallery_view
-from gallery.views import gallery_view
+from django.conf.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
